ea8ec80bc2bfd99ea0cd2adae15a2d2a3ac3ba41
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-07-13 21:15:27 -0400

    Update phonenode.sh

--- phonenode.sh
+++ phonenode.sh
@@ -5,7 +5,7 @@ echo "Downloading stuff... please wait :)"
 apt-get update -qq && apt-get upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qq
 clear
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" openjdk11 python3 wget tmux curl -qq
-ckear
+clear
 curl https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/master/alpine-node-install.sh >> ergo.sh
 sleep 2
 clear
