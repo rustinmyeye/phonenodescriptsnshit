b0b2dda523128550c3a9c94aa2967c7ac16501c0
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-07-13 22:13:27 -0400

    Update phonenode.sh

--- phonenode.sh
+++ phonenode.sh
@@ -3,14 +3,10 @@ clear
 #download things
 echo "Downloading stuff... please wait :)"
 apt-get update -qq && apt-get upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qq
-clear
 apt-get install -y -qq openjdk-17 python3 wget tmux
 clear
 curl https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/master/alpine-node-install.sh >> ergo
 sleep 2
 clear
 chmod +x ergo
-clear
-tmux
-bash ergo
-clear
+tmux new-session  -s ergo_session 'bash ergo'
